7d858cbac0daf0e714df1a6357d2e4cf422d88fa
Avik Nandi <aviknandi@yahoo.com>
2019-09-05 12:57:11 +0530

    date logic completed

--- src/views/ShowData.vue
+++ src/views/ShowData.vue
@@ -1,63 +1,127 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col cols="12" md="4">
-        <v-card elevation = "9" class="cardBorder">
+      <v-col cols="12" md="5">
+        <v-card elevation="9" class="cardBorder">
           <v-card-title class="cardTitle">Input Data</v-card-title>
           <div class="cardContent">
-            <p><b>Machine: </b> {{formData.companyName}}</p>
-            <p><b>Group: </b> {{formData.group}}</p>
-            <p><b>Stat: </b> {{formData.stat}}</p>
-            <p><b>Threshold: </b> {{formData.threshold}}</p>
+            <p>
+              <span>
+                <v-icon color="primary" size="20" style="padding-right:5px">mdi-briefcase</v-icon>
+              </span>
+              <b>Company:&nbsp;</b>
+              {{formData.companyName}}
+            </p>
+            <p>
+              <span>
+                <v-icon color="primary" size="20" style="padding-right:5px">mdi-settings</v-icon>
+              </span>
+              <b>Machine:&nbsp;</b>
+              {{formData.machine}}
+            </p>
+            <p>
+              <span>
+                <v-icon color="primary" size="20" style="padding-right:5px">mdi-cube</v-icon>
+              </span>
+              <b>Group:&nbsp;</b>
+              {{formData.group}}
+            </p>
+            <p>
+              <span>
+                <v-icon color="primary" size="20" style="padding-right:5px">mdi-math-compass</v-icon>
+              </span>
+              <b>Stat:&nbsp;</b>
+              {{formData.stat}}
+            </p>
+            <p>
+              <span>
+                <v-icon color="primary" size="20" style="padding-right:5px">mdi-flash</v-icon>
+              </span>
+              <b>Threshold:&nbsp;</b>
+              {{formData.threshold}}
+            </p>
           </div>
         </v-card>
       </v-col>
-      <v-col cols="12" md="8">
-        <v-card height = "288px" elevation = "7" class="cardBorder" >
+      <v-col cols="12" md="7">
+        <v-card elevation="7" class="cardBorder timeCardHeight">
           <v-card-title class="cardTitle">Input Time</v-card-title>
-          <div v-if = "quickTime" class="cardContent">
-            <p><span style="font-size:15px;padding: 10px 17px; background: aliceblue; margin-right: 28px;">IST:</span> 3/8/2019 12:21</p>
-            <p><span style="font-size:14px;padding: 10px 15px; background: aliceblue; margin-right: 31px;">UTC:</span>3/8/2019 12:21</p>
-            <p><span style="font-size:14px;padding: 10px 16px; background: aliceblue; margin-right: 30px;">EST:</span>3/8/2019 12:21</p>
+          <div v-if="quickTime" class="cardContent">
+            <table>
+              <tr>
+                <td class="text-center" style="font-size:14px;color:#1976d2;font-weight: bold;padding: 10px 17px;background:aliceblue;">LOCAL</td>
+                <td class="time">{{quickTimeLocal | moment("DD-MM-YYYY HH:mm:a")}}</td>
+              </tr>
+              <tr>
+                <td class="text-center" style="font-size:14px;color:#1976d2;font-weight: bold;padding: 10px 17px;background:aliceblue;">UTC</td>
+                <td class="time">{{quickTimeUtc | moment("DD-MM-YYYY HH:mm:a")}}</td>
+              </tr>
+              <tr>
+                <td class="text-center" style="font-size:14px;color:#1976d2;font-weight: bold;padding: 10px 17px;background:aliceblue;">EST</td>
+                <td class="time">{{quickTimeEst | moment("DD-MM-YYYY HH:mm:a")}}</td>
+              </tr>
+            </table>
           </div>
-          <div v-if = "customTime" class="cardContent">
-            <p><span style="font-size:15px;padding: 10px 17px;  margin-right: 28px;"></span> <span style="margin: 0 70px;">To</span><span style="margin: 0 30px;">From</span></p>
-            <p><span style="font-size:15px;padding: 10px 17px; background: aliceblue; margin-right: 28px;">IST:</span> <span>{{localDateTimeFrom | moment("DD-MM-YYYY HH:mm:a")}}</span><span style="margin-left:20px">{{localDateTimeTo | moment("DD-MM-YYYY HH:mm:a")}}</span></p>
-             <p><span style="font-size:14px;padding: 10px 15px; background: aliceblue; margin-right: 31px;">UTC:</span> <span>{{utcDateTimeFrom | moment("DD-MM-YYYY HH:mm:a")}}</span><span style="margin-left:20px">{{utcDateTimeTo | moment("DD-MM-YYYY HH:mm:a")}}</span></p>
-            <p><span style="font-size:14px;padding: 10px 16px; background: aliceblue; margin-right: 30px;">EST:</span> <span>{{estDateTimeFrom | moment("DD-MM-YYYY HH:mm:a")}}</span><span style="margin-left:20px">{{estDateTimeTo | moment("DD-MM-YYYY HH:mm:a")}}</span></p>
+          <div v-if="customTime" class="cardContent">
+            <table>
+              <tr>
+                <th></th>
+                <th>To</th>
+                <th>From</th>
+              </tr>
+              <tr>
+                <td class="text-center" style="font-size:15px;color:#1976d2;font-weight: bold;padding: 10px 17px;background:aliceblue;">LOCAL</td>
+                <td class="time" style="border-top:1px solid #f2f2f2">{{localDateTimeFrom | moment("DD-MM-YYYY HH:mm:a")}}</td>
+                <td class="time" style="border-top:1px solid #f2f2f2">{{localDateTimeTo | moment("DD-MM-YYYY HH:mm:a")}}</td>
+              </tr>
+              <tr>
+                <td class="text-center" style="font-size:14px;color:#1976d2;font-weight: bold;padding: 10px 17px;background:aliceblue">UTC</td>
+                <td class="time">{{utcDateTimeFrom | moment("DD-MM-YYYY HH:mm:a")}}</td>
+                <td class="time">{{utcDateTimeTo | moment("DD-MM-YYYY HH:mm:a")}}</td>
+              </tr>
+              <tr>
+                <td class="text-center" style="font-size:14px;color:#1976d2;font-weight: bold;padding: 10px 17px;background:aliceblue">EST</td>
+                <td class="time">{{estDateTimeFrom | moment("DD-MM-YYYY HH:mm:a")}}</td>
+                <td class="time">{{estDateTimeTo | moment("DD-MM-YYYY HH:mm:a")}}</td>
+              </tr>
+            </table>
           </div>
         </v-card>
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12" md="3" v-for="item in buttonList"  :key="item.name">
+      <v-col cols="12" md="3" v-for="item in buttonList" :key="item.name">
         <div class="text-center">
-        <v-btn :color="item.color" medium min-width="220px" :to="item.url">
-          {{item.name}}
-        </v-btn>
-
+          <v-btn :color="item.color" medium min-width="220px" :to="item.url">{{item.name}}</v-btn>
         </div>
       </v-col>
     </v-row>
   </v-container>
-  
 </template>
 
 <style lang = "scss" scoped>
-  .cardTitle{
-    border-bottom:1px solid #8080803d
-  }
-  .cardContent p{
-    border-bottom:1px solid #8080803d;
-    color: #363535b9;
-    padding-bottom: 7px;
-  }
-  .cardContent{
-    padding: 20px;
-  }
-  .cardBorder{
-    border-radius: 7px;
-  }
+.cardTitle {
+  border-bottom: 1px solid #8080803d;
+}
+.cardContent p {
+  border-bottom: 1px solid #8080803d;
+  color: #363535b9;
+  padding-bottom: 7px;
+}
+.cardContent {
+  padding: 20px;
+}
+.cardBorder {
+  border-radius: 7px;
+}
+.time{
+  font-size:15px;
+  padding: 10px 17px;
+  border-bottom: 1px solid #f2f2f2;
+}
+/* .timeCardHeight{
+  height: 336px;
+} */
 </style>
 
 <script lang="ts">
@@ -66,57 +130,120 @@ import { Component } from "vue-property-decorator";
 
 @Component
 export default class ShowData extends Vue {
+  formData!: any;
+  quickTime = false;
+  customTime = false;
+  buttonList = [
+    { name: "Data Table", color: "success", url: "data-view/data-table" },
+    { name: "Line Plot", color: "primary", url: "data-view/line-plot" },
+    { name: "FFT", color: "warning", url: "data-view/fft" },
+    { name: "PSD", color: "info", url: "data-view/psd" }
+  ];
 
-    formData!:any;
-    quickTime = false;
-    customTime = false;
-    buttonList = [
-      { name: "Data Table" , color: "success", url: "data-view/data-table" },
-      { name: "Line Plot", color: "primary", url: "data-view/line-plot" },
-      { name: "FFT", color: "warning", url: "data-view/fft" },
-      { name: "PSD", color: "info", url: "data-view/psd"}
-    ]
+  //time variables
+  quickTimeLocal?:any;
+  quickTimeUtc?:any;
+  quickTimeEst?:any;
 
-    //time variables
-    localDateTimeFrom?:any
-    utcDateTimeFrom?:any
-    estDateTimeFrom?:any
+  localDateTimeFrom?: any;
+  utcDateTimeFrom?: any;
+  estDateTimeFrom?: any;
 
-    localDateTimeTo?:any
-    utcDateTimeTo?:any
-    estDateTimeTo?:any
-    // end
-    
-    created(){
-      console.log(this.$store.state.formData)
-      this.formData = this.$store.state.formData;
-      if(!this.formData.quickTime){
-        this.customTime = true;
-        let x = this.timeConvertCustom(this.$store.state.formData.fromDate,this.$store.state.formData.fromHourMinutes);
-        this.localDateTimeFrom = x.localDateTime;
-        this.utcDateTimeFrom = x.utcDateTime;
-        this.estDateTimeFrom = x.estDateTime;
+  localDateTimeTo?: any;
+  utcDateTimeTo?: any;
+  estDateTimeTo?: any;
+  // end
 
-        let y = this.timeConvertCustom(this.$store.state.formData.toDate,this.$store.state.formData.toHourMinutes);
-        this.localDateTimeTo = y.localDateTime;
-        this.utcDateTimeTo = y.utcDateTime;
-        this.estDateTimeTo = y.estDateTime;
-      } else{
-        this.quickTime = true;
-      }     
+  created() {
+    console.log(this.$store.state.formData);
+    this.formData = this.$store.state.formData;
+    if (!this.formData.quickTime) {
+      this.customTime = true;
+      let x = this.timeConvertCustom(
+        this.$store.state.formData.fromDate,
+        this.$store.state.formData.fromHourMinutes
+      );
+      this.localDateTimeFrom = x.localDateTime;
+      this.utcDateTimeFrom = x.utcDateTime;
+      this.estDateTimeFrom = x.estDateTime;
+
+      let y = this.timeConvertCustom(
+        this.$store.state.formData.toDate,
+        this.$store.state.formData.toHourMinutes
+      );
+      this.localDateTimeTo = y.localDateTime;
+      this.utcDateTimeTo = y.utcDateTime;
+      this.estDateTimeTo = y.estDateTime;
+    } else {
+      this.quickTime = true;
+      let timeValueArray = this.formData.quickTime.split("")
+      let i = timeValueArray[timeValueArray.length-1];
+      timeValueArray.pop();
+      let min = timeValueArray.join("");
+      console.log(min,i)
+      let quickTimeData = this.quickTimeConvert(parseInt(min),i);
+      this.quickTimeLocal = quickTimeData.quickLocalDateTime;
+      this.quickTimeUtc = quickTimeData.quickUtcDateTime;
+      this.quickTimeEst = quickTimeData.quickEstDateTime;
+      
     }
+  }
 
-    timeConvertCustom(date:string,time:string){
-      let localDate = new Date(date+" "+time); 
+  timeConvertCustom(date: string, time: string) {
+    let localDate = new Date(date + " " + time);
 
-      let utcDate = localDate.getTime() + (localDate.getTimezoneOffset() * 60000);
-      let estDate = new Date(utcDate + (3600000*-5.0));
+    let utcDate = localDate.getTime() + localDate.getTimezoneOffset() * 60000;
+    let estDate = new Date(utcDate + 3600000 * -5.0);
+    let result = {
+      localDateTime: localDate,
+      utcDateTime: new Date(utcDate),
+      estDateTime: estDate
+    };
+    return result;
+  }
+
+  quickTimeConvert(min:number,i:string){
+    // let i = "d";
+    // let min = 2;
+    if (i === "m") {
+      let timedate = new Date(Date.now() - 60000 * min);
+      let utcDate = timedate.getTime() + timedate.getTimezoneOffset() * 60000;
+      let estDate = new Date(utcDate + 3600000 * -5.0);
       let result = {
-        localDateTime: localDate,
-        utcDateTime: new Date(utcDate),
-        estDateTime: estDate  
-      }
-      return(result);
+        quickLocalDateTime: timedate,
+        quickUtcDateTime: new Date(utcDate),
+        quickEstDateTime: estDate
+      };
+      return result;
+
+    } else if (i === "h") {
+      let timedate = new Date(Date.now() - 1000 * 3600 * min);
+      let utcDate = timedate.getTime() + timedate.getTimezoneOffset() * 60000;
+      let estDate = new Date(utcDate + 3600000 * -5.0);
+      let result = {
+        quickLocalDateTime: timedate,
+        quickUtcDateTime: new Date(utcDate),
+        quickEstDateTime: estDate
+      };
+      return result;
+    } else if (i === "d") {
+      let timedate = new Date(Date.now() - 1000 * 3600 * 24 * min);
+      let utcDate = timedate.getTime() + timedate.getTimezoneOffset() * 60000;
+      let estDate = new Date(utcDate + 3600000 * -5.0);
+      let result = {
+        quickLocalDateTime: timedate,
+        quickUtcDateTime: new Date(utcDate),
+        quickEstDateTime: estDate
+      };
+      return result;
     }
+
+
+
+    let localDate = new Date();
+
+    
+  }
 }
 </script>
+
